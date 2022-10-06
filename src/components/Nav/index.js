dda808391778a0bdcad8f64d5cb95ddb03e88315
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-10-06 10:57:13 -0300

    Feat: Criei um menu na nav para telas menores

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,12 +5,17 @@ import SettingsDarkImgURL from '../../assets/icons/settings-dark.png'
 import SettingsImgURL from '../../assets/icons/settings.png'
 import MoonImgURL from '../../assets/icons/moon.png'
 import SunImgURL from '../../assets/icons/sun.png'
+import CloseImgURL from '../../assets/icons/close.png'
+import CloseDarkImgURL from '../../assets/icons/close-dark.png'
+import MenuImgURL from '../../assets/icons/menu.png'
+import MenuDarkImgURL from '../../assets/icons/menu-dark.png'
 
-import { Container, Logo, NavBar, ContainerSettings, SwitchTheme, SwitchSettings } from "./styles"
+import { Container, Logo, NavBar, ContainerSettings, SwitchTheme, SwitchSettings, ContainerMenu, IconMenu, ButtonClose } from "./styles"
 
 export function Nav() {
   const { theme, handleThemes } = useContext(ThemeContext)
   const [themeVisible, setThemeVisible] = useState(false)
+  const [menuVisible, setMenuVisible] = useState(false)
 
   function showThemes() {
     themeVisible === true ? setThemeVisible(false) : setThemeVisible(true)
@@ -18,16 +23,51 @@ export function Nav() {
 
   return (
     <Container>
+      {menuVisible && <ContainerMenu>
+        <ButtonClose onClick={() => { setMenuVisible(false) }}>
+         
+          {theme.title == 'dark' ? (
+         <img src={CloseImgURL} alt="Fechar menu" />
+      ) : (
+        <img src={CloseDarkImgURL} alt="Fechar menu" />
+      )}
+        </ButtonClose>
+        <nav>
+          <a href="#Home">Home</a>
+          <a href="#AboutMe">About me</a>
+          <a href="#Skills">Skills</a>
+          <a href="#Contact">Contact</a>
+        </nav>
+        <div>
+          <button>
+            {theme.title == 'dark' ? (
+              <img src={SunImgURL} onClick={handleThemes} alt="ícone de tema" />
+            ) : (
+              <img src={MoonImgURL} onClick={handleThemes} alt="ícone de tema" />
+            )}
+          </button>
+        </div>
+      </ContainerMenu>}
+
+      {theme.title == 'dark' ? (
+        <IconMenu onClick={() => { setMenuVisible(true) }} src={MenuImgURL} alt="Ícone do menu" />
+      ) : (
+        <IconMenu onClick={() => { setMenuVisible(true) }} src={MenuDarkImgURL} alt="Ícone do menu" />
+      )}
+
       <Logo>EB</Logo>
+
       <NavBar>
         <a href="#Home">Home</a>
         <a href="#AboutMe">About me</a>
         <a href="#Skills">Skills</a>
         <a href="#Contact">Contact</a>
       </NavBar>
+
       <span> </span>
       <span> </span>
       <span> </span>
+
       <ContainerSettings>
         <SwitchTheme isVisible={themeVisible}>
           <button>
--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components"
+import styled from "styled-components"
 
 export const Container = styled.nav`
   z-index: 10;
@@ -12,14 +12,9 @@ export const Container = styled.nav`
   justify-content: space-between;
   align-items: center;
 
-  div{
-    margin-right: 14px;
-
-    img{
-      width: 30px;
-    }
+  @media screen and (max-width:  768px){
+    padding: 0px;
   }
-  
 `
 
 export const Logo = styled.div`
@@ -30,11 +25,19 @@ export const Logo = styled.div`
   font-size: 40px;
   color: ${ ({theme}) => theme.colors.colorBgSecondary};
   background-color: var(--color-main);
+
+  @media screen and (max-width:  768px){
+    display: none;
+  }
 `
 
 export const NavBar = styled.div`
   display: flex; 
   gap: 20px;
+
+  @media screen and (max-width:  768px){
+    display: none;
+  }
 
   a{
     font-size: 18px;
@@ -53,9 +56,10 @@ export const ContainerSettings = styled.div`
   display: flex;
   gap: 10px;
 
+  @media screen and (max-width:  768px){
+    display: none;
+  }
 `
-
-
 
 export const SwitchSettings = styled.button`
   background-color: #00000000;
@@ -66,6 +70,7 @@ export const SwitchSettings = styled.button`
   }
 
   img{
+    width: 30px;
     z-index: 5;
     cursor: pointer;
   }
@@ -77,6 +82,10 @@ export const SwitchTheme = styled.div`
   bottom: ${({isVisible})=> isVisible === true ? '-1px' : '60px'};
   transition: bottom 1s ease;
 
+  img{
+    width: 30px;
+  }
+
   button{
     cursor: pointer;
     background-color: #00000000;
@@ -86,4 +95,81 @@ export const SwitchTheme = styled.div`
       transform: scale(0.90);
     }
   }
+`
+
+export const ContainerMenu = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  height: 100vh;
+  width: 180px;
+
+  background-color: ${ ({ theme }) => theme.colors.colorBg};
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  box-shadow:${ ({ theme }) => theme.title === 'dark' ? '-5px 0px 15px black inset' : '-5px 0px 15px var(--color-tertiary) inset'};
+
+  nav{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    margin-top: 10px;
+
+    a{
+      font-size: 18px;
+      font-weight: 700;
+      color:${ ({theme}) => theme.colors.colorTextSecondary};
+
+      :hover{
+        text-decoration: underline;
+        color: ${ ({theme}) => theme.colors.colorBgSecondary};
+      }
+    }
+  }
+
+  div{
+    align-self: flex-end;
+
+    button{
+      background-color: #00000000;
+      border: none;
+
+      :active{
+        transform: scale(0.90);
+      }
+      img{
+        width: 30px;
+      }
+    }
+  }
+`
+
+export const IconMenu = styled.img`
+  width: 30px;
+  margin: 10px;
+
+  @media screen and (min-width:  769px){
+    display: none;
+  }
+`
+
+export const ButtonClose = styled.button`
+   position: absolute;
+    right: 8px;
+    top: 8px;
+
+    background-color: #00000000;
+    border: none;
+
+    :active{
+      transform: scale(0.90);
+    }
+
+    img{
+      width: 20px;
+    }
 `
