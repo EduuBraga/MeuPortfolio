e3ade2580b8f80f5b138d9f03ff83071dcf02fbb
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-10-07 16:23:35 -0300

    Revert "Chore: Adicionei os links as referências"
    This reverts commit c62b37c8194aaac3815ba4b43ff20e741f439b86.

--- src/components/Skills/styles.js
+++ src/components/Skills/styles.js
@@ -85,17 +85,7 @@ export const ContainerTop = styled.div`
 
 export const ContainerBottom = styled.div`
   font-size: 18px;
-
   @media screen and (max-width: 426px){
     font-size: 16px;
-  }
-  a{
-    text-decoration: underline;
-    text-decoration-color: var(--color-secondary) ;
-    transition: all 0.2s linear;
-
-    :hover{
-      background-color: black;
-    }
   }
 `
