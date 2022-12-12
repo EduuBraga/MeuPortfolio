4458904bfe8adfec9ae65e911e3290fe034ed259
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-12 19:29:45 -0300

    Feat: Add techs utilizadas em cada projeto

--- src/Services/projects.js
+++ src/Services/projects.js
@@ -4,85 +4,96 @@ import CursoMarktingImgURl from '../assets/ImgsProjects/projeto-cursomarketing.j
 import PlaylistDjongaImgURl from '../assets/ImgsProjects/projeto-playlistdjonga.jpg';
 import EnygmaMusicImgURl from '../assets/ImgsProjects/projeto-enygmamusic.jpg';
 import OnepieceImgURl from '../assets/ImgsProjects/projeto-onepiece.jpg';
-import TodoListImgURl from '../assets/ImgsProjects/projeto-todolist.jpg';
 import ColdTechImgURl from '../assets/ImgsProjects/projeto-coldtech.jpg';
 import ControleDeFinancasImgURl from '../assets/ImgsProjects/controle_de_financas.jpg';
 import PhotoSnapImgURl from '../assets/ImgsProjects/photosnap.jpg';
 import PomodoroImgURl from '../assets/ImgsProjects/pomodoro.jpg';
-import todoList from '../assets/ImgsProjects/todo-list.jpg';
-import donwloadThumbnails from '../assets/ImgsProjects/donwload-thumbnails-yt.jpg';
+import todoListImgURl from '../assets/ImgsProjects/todo-list.jpg';
+import donwloadThumbnailsImgURL from '../assets/ImgsProjects/donwload-thumbnails-yt.jpg';
 
 export const projectsFiles = [
   {
     name: 'e-commerce - ColdTech',
     deploy: 'https://e-commerce-cold-tech.vercel.app/',
     img: ColdTechImgURl,
-    repo: 'https://github.com/EduuBraga/E-commerce-ColdTech'
-  },
-  {
-    name: 'LandingPage - Curso Markting Digital',
-    deploy: 'https://curso-marketingdigital.netlify.app/',
-    img: CursoMarktingImgURl,
-    repo: 'https://github.com/EduuBraga/LandingPage-MarketingDigital'
+    repo: 'https://github.com/EduuBraga/E-commerce-ColdTech',
+    techs: 'ReactJS, Styled-Components, React-Router-Dom, ContextAPI & JavaScript'
   },
   {
     name: 'Controle de Finanças',
     deploy: 'https://controlefinanceiro-five.vercel.app/',
     img: ControleDeFinancasImgURl,
-    repo: 'https://github.com/EduuBraga/ControleFinanceiro'
+    repo: 'https://github.com/EduuBraga/ControleFinanceiro',
+    techs: 'ReactJS, Styled-Components, ContextAPI & JavaScript'
   },
   {
     name: 'Photosnap',
     deploy: 'https://photosnap-platform.vercel.app/',
     img: PhotoSnapImgURl,
-    repo: 'https://github.com/EduuBraga/Photosnap'
+    repo: 'https://github.com/EduuBraga/Photosnap',
+    techs: 'ReactJS, Styled-Components & JavaScript'
   },
   {
     name: 'Pomodoro APP',
     deploy: 'https://pomodoro-study.vercel.app/',
     img: PomodoroImgURl,
-    repo: 'https://github.com/EduuBraga/PomodoroAPP'
+    repo: 'https://github.com/EduuBraga/PomodoroAPP',
+    techs: 'ReactJS, Styled-Components, ContextAPI & JavaScript'
   },
   {
     name: 'To Do list',
     deploy: 'https://todo-list-eduubraga.vercel.app/',
-    img: todoList,
-    repo: 'https://github.com/EduuBraga/todo-list'
+    img: todoListImgURl,
+    repo: 'https://github.com/EduuBraga/todo-list',
+    techs: 'ReactJS, Styled-Components, ContextAPI & JavaScript'
   },
   {
     name: 'Download Thumbnails YouTube',
     deploy: 'https://download-thumbnail-yt.netlify.app/',
-    img: donwloadThumbnails,
-    repo: 'https://github.com/EduuBraga/downloadThumbnailsYT'
+    img: donwloadThumbnailsImgURL,
+    repo: 'https://github.com/EduuBraga/downloadThumbnailsYT',
+    techs: 'JavaScript, HTML & CSS'
   },
   {
     name: 'Jogo da Memória',
     deploy: 'https://react-jogo-da-memoria.vercel.app/',
     img: JogoDaMemoriaImgURl,
-    repo: 'https://github.com/EduuBraga/React-JogoDaMemoria'
+    repo: 'https://github.com/EduuBraga/React-JogoDaMemoria',
+    techs: 'ReactJS, JavaScript, HTML & CSS'
+  },
+  {
+    name: 'LandingPage - Curso Markting Digital',
+    deploy: 'https://curso-marketingdigital.netlify.app/',
+    img: CursoMarktingImgURl,
+    repo: 'https://github.com/EduuBraga/LandingPage-MarketingDigital',
+    techs: 'JavaScript, HTML & CSS'
   },
   {
     name: 'Jogo da Velha',
     deploy: 'https://jogodavelha-edubraga.netlify.app/',
     img: JogoDaVelhaImgURl,
-    repo: 'https://github.com/EduuBraga/JogoDaVelha'
+    repo: 'https://github.com/EduuBraga/JogoDaVelha',
+    techs: 'JavaScript, HTML & CSS'
   },
   {
     name: 'Playlist Djonga',
     deploy: 'https://djonga-playlist.netlify.app/',
     img: PlaylistDjongaImgURl,
-    repo: 'https://github.com/EduuBraga/PlayListDjonga'
+    repo: 'https://github.com/EduuBraga/PlayListDjonga',
+    techs: 'JavaScript, HTML & CSS'
   },
   {
     name: 'Enygma Songs',
     deploy: 'https://enygmamusic.netlify.app/',
     img: EnygmaMusicImgURl,
-    repo: 'https://github.com/EduuBraga/EnygmaSongs'
+    repo: 'https://github.com/EduuBraga/EnygmaSongs',
+    techs: 'JavaScript, HTML & CSS'
   },
   {
     name: 'One Piece - Personagens',
     deploy: 'https://onepiecepersonagens.netlify.app/',
     img: OnepieceImgURl,
-    repo: 'https://github.com/EduuBraga/OnePiece-Personagens'
+    repo: 'https://github.com/EduuBraga/OnePiece-Personagens',
+    techs: 'JavaScript, HTML & CSS'
   },
 ];
